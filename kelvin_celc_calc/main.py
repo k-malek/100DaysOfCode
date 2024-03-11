7c7f84f243b2de2a7cdb46cbdfd59af2e492a822
k-malek <krzysztof.malek.90@gmail.com>
2024-03-11 16:41:29 +0100

    Code clean-up for Kelvin to Celsius converter

--- kelvin_celc_calc/main.py
+++ kelvin_celc_calc/main.py
@@ -1,7 +1,8 @@
 import tkinter
 import tkinter.ttk as ttk
 
-app_font=("Arial Narrow",12,"bold")
+APP_FONT=("Arial Narrow",12,"bold")
+GRID_ELEMENTS_PADDING_X,GRID_ELEMENTS_PADDING_Y=(20,10)
 
 def kelv_to_cel_calc(present,amount):
     try:
@@ -13,20 +14,18 @@ def kelv_to_cel_calc(present,amount):
 
 
 window = tkinter.Tk()
-
-
 window.title("Kelvin to Celsius converter")
-#window.minsize(width=500,height=300)
+window.config(padx=30,pady=20)
 
-kelvin_input=ttk.Entry(window)
-kelvin_label = ttk.Label(window,text="How many Kelvins?",font=app_font)
-result_label = ttk.Label(window,font=app_font)
+kelvin_input=ttk.Entry(window, width=8)
+kelvin_label = ttk.Label(window,text="How many Kelvins to convert?",font=APP_FONT)
+result_label = ttk.Label(window,text="Result will appear here", font=APP_FONT)
 button = ttk.Button(window,text="Convert",command=lambda: kelv_to_cel_calc(result_label,kelvin_input.get()))
 
-kelvin_input.grid(row=0,column=1)
-kelvin_label.grid(row=0,column=0)
-button.grid(row=1,column=1)
-result_label.grid(row=2, column=0, columnspan=2)
+kelvin_input.grid(row=0,column=1, padx=GRID_ELEMENTS_PADDING_X, pady=GRID_ELEMENTS_PADDING_Y)
+kelvin_label.grid(row=0,column=0, padx=GRID_ELEMENTS_PADDING_X, pady=GRID_ELEMENTS_PADDING_Y)
+button.grid(row=1,column=0,columnspan=2, padx=GRID_ELEMENTS_PADDING_X, pady=GRID_ELEMENTS_PADDING_Y)
+result_label.grid(row=2, column=0, columnspan=2, padx=GRID_ELEMENTS_PADDING_X, pady=GRID_ELEMENTS_PADDING_Y)
 
 window.bind('<Return>',lambda x: kelv_to_cel_calc(result_label,kelvin_input.get()))
 window.mainloop()
